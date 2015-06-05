063da8c9cf430984a914b3f5a4f006d29df8d6fa
StarZou <zoutongyuan@163.com>
2015-06-05 10:32:27 +0800

    Initial commit

---
+++ angular-validation.js
@@ -0,0 +1,14 @@
+/**
+ * @module ngValidation
+ * @description A high-performance and high-availability form validation component based on AngularJS
+ * @author StarZou
+ * @email tongyuanzou@gmail.com
+ * @time 2015
+ * @license MIT
+ **/
+(function (window, angular, undefined) {
+    'use strict';
+
+    angular.module('ngValidation', ['ng']);
+
+})(window, window.angular);
