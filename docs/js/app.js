df9cfa90e2359fdfc72c1b0e31b3332e0d88f910
StarZou <zoutongyuan@163.com>
2015-06-05 14:43:34 +0800

    update docs

--- docs/js/app.js
+++ docs/js/app.js
@@ -18,4 +18,48 @@
         $scope.title = 'ngValidation Documentation and Examples';
     }]);
 
+    /**
+     * 菜单指令
+     */
+    ngValidationDocsModule.directive('menu', [function () {
+        return {
+            restrict   : 'E',
+            replace    : true,
+            scope      : true,
+            templateUrl: 'templates/menu.tpl.html',
+            link       : function ($scope, $element, $attrs) {
+                $scope.menuList = [
+                    {
+                        type : 'docs',
+                        menus: [
+                            {
+                                title: '概述', state: 'docs.overview'
+                            },
+                            {
+                                title: '通用表单验证', state: 'docs.general'
+                            },
+                            {
+                                title: '动态表单验证', state: 'docs.dynamic'
+                            },
+                            {
+                                title: '分组类型验证', state: 'docs.group'
+                            },
+                            {
+                                title: '高级扩展', state: 'docs.advanced'
+                            }
+                        ]
+                    },
+                    {
+                        type : 'me',
+                        menus: [
+                            {
+                                title: '关于', state: 'me.about'
+                            }
+                        ]
+                    }
+                ];
+            }
+        };
+    }]);
+
 })(window, window.angular);
