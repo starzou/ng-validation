039b74d31a12d269738ef1cd6d8877b53bdc8c4d
StarZou <zoutongyuan@163.com>
2015-06-06 15:59:46 +0800

    dev

--- angular-validation.js
+++ angular-validation.js
@@ -52,16 +52,17 @@
 
         provider('$tooltip', function () {
             var defaults = this.defaults = {
-                show    : true,
-                template: [
-                    '<div class="tooltip right fade in" ng-show="show && title">',
-                    '<div class="tooltip-arrow" style="border-right-color: #d9534f;"></div>',
-                    '<div class="tooltip-inner" style="background-color: #d9534f;" ng-bind="title"></div>',
+                show     : true,
+                placement: 'top',
+                template : [
+                    '<div class="tooltip in" ng-show="show && title">',
+                    '<div class="tooltip-arrow"></div>',
+                    '<div class="tooltip-inner" ng-bind="title"></div>',
                     '</div>'
                 ].join('')
             };
 
-            this.$get = ['$rootScope', '$compile', '$timeout', function ($rootScope, $compile, $timeout) {
+            this.$get = ['$rootScope', '$compile', '$timeout', '$dimensions', function ($rootScope, $compile, $timeout, $dimensions) {
                 function TooltipFactory($element, config) {
                     var $tooltip = {},
                         options = $tooltip.$options = angular.extend({}, defaults, config),
@@ -86,10 +87,30 @@
                         });
                     };
 
+                    $tooltip.position = function () {
+                        var elementPosition = $dimensions.getPosition($element[0], false);
+
+                        var tooltipWidth = tooltipElement.prop('offsetWidth'),
+                            tooltipHeight = tooltipElement.prop('offsetHeight');
+
+                        var tooltipPosition = $dimensions.getCalculatedOffset(options.placement, elementPosition, tooltipWidth, tooltipHeight);
+
+                        tooltipPosition.top += 'px';
+                        tooltipPosition.left += 'px';
+
+                        tooltipElement.css(tooltipPosition);
+                    };
+
                     $tooltip.init = function () {
                         this.setScope(options);
                         tooltipLinker(scope);
+
+                        tooltipElement.css({top: '-9999px', left: '-9999px'});
+                        tooltipElement.addClass(options.placement);
+
                         $element.after(tooltipElement);
+
+                        $timeout(this.position);
                     };
 
                     $tooltip.show = function (option) {
@@ -266,6 +287,221 @@
                 }
             }
 
+        }]).
+
+        factory('$dimensions', ['$document', '$window', function ($document, $window) {
+
+            var $dimensions = {};
+
+            /**
+             * 确定元素 节点名
+             * @param element
+             * @param nodeName
+             */
+            var nodeName = $dimensions.nodeName = function (element, name) {
+                return element.nodeName && element.nodeName.toLowerCase() === name.toLowerCase();
+            };
+
+            /**
+             * 根据属性名, 返回元素 计算的属性值
+             * @param element
+             * @param property
+             * @param extra
+             */
+            $dimensions.css = function (element, property, extra) {
+                var value;
+                if (element.currentStyle) { //IE
+                    value = element.currentStyle[property];
+                } else if (window.getComputedStyle) {
+                    value = window.getComputedStyle(element)[property];
+                } else {
+                    value = element.style[property];
+                }
+                return extra === true ? parseFloat(value) || 0 : value;
+            };
+
+            /**
+             * 返回元素 偏移量 offset
+             * @param element
+             * @returns {{width: (Number|number), height: (Number|number), top: number, left: number}}
+             */
+            $dimensions.offset = function (element) {
+                var boxRect = element.getBoundingClientRect();
+                var docElement = element.ownerDocument;
+                return {
+                    width : boxRect.width || element.offsetWidth,
+                    height: boxRect.height || element.offsetHeight,
+                    top   : boxRect.top + (window.pageYOffset || docElement.documentElement.scrollTop) - (docElement.documentElement.clientTop || 0),
+                    left  : boxRect.left + (window.pageXOffset || docElement.documentElement.scrollLeft) - (docElement.documentElement.clientLeft || 0)
+                };
+            };
+
+            /**
+             * 返回元素 位置 position
+             * @param element
+             * @returns {{width: number, height: number, top: number, left: number}}
+             */
+            $dimensions.position = function (element) {
+
+                var offsetParentRect = {top: 0, left: 0},
+                    offsetParentElement,
+                    offset;
+
+                // Fixed elements are offset from window (parentOffset = {top:0, left: 0}, because it is it's only offset parent
+                if ($dimensions.css(element, 'position') === 'fixed') {
+
+                    // We assume that getBoundingClientRect is available when computed position is fixed
+                    offset = element.getBoundingClientRect();
+
+                } else {
+
+                    // Get *real* offsetParentElement
+                    offsetParentElement = offsetParent(element);
+
+                    // Get correct offsets
+                    offset = $dimensions.offset(element);
+                    if (!nodeName(offsetParentElement, 'html')) {
+                        offsetParentRect = $dimensions.offset(offsetParentElement);
+                    }
+
+                    // Add offsetParent borders
+                    offsetParentRect.top += $dimensions.css(offsetParentElement, 'borderTopWidth', true);
+                    offsetParentRect.left += $dimensions.css(offsetParentElement, 'borderLeftWidth', true);
+                }
+
+                // Subtract parent offsets and element margins
+                return {
+                    width : element.offsetWidth,
+                    height: element.offsetHeight,
+                    top   : offset.top - offsetParentRect.top - $dimensions.css(element, 'marginTop', true),
+                    left  : offset.left - offsetParentRect.left - $dimensions.css(element, 'marginLeft', true)
+                };
+
+            };
+
+            /**
+             * Returns the closest, non-statically positioned offsetParent of a given element
+             * @required-by fn.position
+             * @param element
+             */
+            var offsetParent = function offsetParentElement(element) {
+                var docElement = element.ownerDocument;
+                var offsetParent = element.offsetParent || docElement;
+                if (nodeName(offsetParent, '#document')) return docElement.documentElement;
+                while (offsetParent && !nodeName(offsetParent, 'html') && $dimensions.css(offsetParent, 'position') === 'static') {
+                    offsetParent = offsetParent.offsetParent;
+                }
+                return offsetParent || docElement.documentElement;
+            };
+
+            /**
+             * Provides equivalent of jQuery's height function
+             * @required-by bootstrap-affix
+             * @url http://api.jquery.com/height/
+             * @param element
+             * @param outer
+             */
+            $dimensions.height = function (element, outer) {
+                var value = element.offsetHeight;
+                if (outer) {
+                    value += $dimensions.css(element, 'marginTop', true) + $dimensions.css(element, 'marginBottom', true);
+                } else {
+                    value -= $dimensions.css(element, 'paddingTop', true) + $dimensions.css(element, 'paddingBottom', true) + $dimensions.css(element, 'borderTopWidth', true) + $dimensions.css(element, 'borderBottomWidth', true);
+                }
+                return value;
+            };
+
+            /**
+             * Provides equivalent of jQuery's width function
+             * @required-by bootstrap-affix
+             * @url http://api.jquery.com/width/
+             * @param element
+             * @param outer
+             */
+            $dimensions.width = function (element, outer) {
+                var value = element.offsetWidth;
+                if (outer) {
+                    value += $dimensions.css(element, 'marginLeft', true) + $dimensions.css(element, 'marginRight', true);
+                } else {
+                    value -= $dimensions.css(element, 'paddingLeft', true) + $dimensions.css(element, 'paddingRight', true) + $dimensions.css(element, 'borderLeftWidth', true) + $dimensions.css(element, 'borderRightWidth', true);
+                }
+                return value;
+            };
+
+
+            /**
+             * 计算 元素 显示的 位置
+             * @param placement
+             * @param position
+             * @param actualWidth
+             * @param actualHeight
+             * @returns {*}
+             */
+            $dimensions.getCalculatedOffset = function getCalculatedOffset(placement, position, actualWidth, actualHeight) {
+                var offset;
+                var split = placement.split('-');
+
+                switch (split[0]) {
+                    case 'right':
+                        offset = {
+                            top : position.top + position.height / 2 - actualHeight / 2,
+                            left: position.left + position.width
+                        };
+                        break;
+                    case 'bottom':
+                        offset = {
+                            top : position.top + position.height,
+                            left: position.left + position.width / 2 - actualWidth / 2
+                        };
+                        break;
+                    case 'left':
+                        offset = {
+                            top : position.top + position.height / 2 - actualHeight / 2,
+                            left: position.left - actualWidth
+                        };
+                        break;
+                    default:
+                        offset = {
+                            top : position.top - actualHeight,
+                            left: position.left + position.width / 2 - actualWidth / 2
+                        };
+                        break;
+                }
+
+                if (!split[1]) {
+                    return offset;
+                }
+
+                if (split[0] === 'top' || split[0] === 'bottom') {
+                    switch (split[1]) {
+                        case 'left':
+                            offset.left = position.left;
+                            break;
+                        case 'right':
+                            offset.left = position.left + position.width - actualWidth;
+                    }
+                } else if (split[0] === 'left' || split[0] === 'right') {
+                    switch (split[1]) {
+                        case 'top':
+                            offset.top = position.top - actualHeight;
+                            break;
+                        case 'bottom':
+                            offset.top = position.top + position.height;
+                    }
+                }
+
+                return offset;
+            };
+
+            $dimensions.getPosition = function getPosition(element, appendToBody) {
+                if (appendToBody) {
+                    return $dimensions.offset(element);
+                } else {
+                    return $dimensions.position(element);
+                }
+            };
+
+            return $dimensions;
         }]);
 
 })(window, window.angular);
